df3ccab9fb87df6703f62de1de63475cef5fbdd4
peter_and_wolf <peter.emelianov@gmail.com>
2024-12-11 18:50:53 +0300

    + train.py

--- src/train.py
+++ src/train.py
@@ -1,9 +1,13 @@
 import logging
+from functools import partial
+
+import mlflow
+from hyperopt import fmin, tpe, hp, STATUS_OK, Trials, SparkTrials
 
 from pyspark.sql import SparkSession
 from pyspark.ml.feature import VectorAssembler, OneHotEncoder, StringIndexer
 from pyspark.ml.classification import LogisticRegression
-from pyspark.ml.tuning import ParamGridBuilder, TrainValidationSplit
+from pyspark.ml.evaluation import BinaryClassificationEvaluator
 from pyspark.ml import Pipeline
 
 
@@ -29,7 +33,7 @@ def data_prep_pipeline(spark):
       'Fare',
       'EmbarkVector'
     ],
-    outputCol='features'
+    outputCol='Features'
   )
 
   return Pipeline(stages=[
@@ -41,20 +45,91 @@ def data_prep_pipeline(spark):
   ])
 
 
-# def train():
+# Определяем пространство поиска для hyperopt
+search_space = {
+  'regParam': hp.lognormal('regParam', 0, 1.0),
+  'fitIntercept': hp.choice('fitIntercept', [False, True])
+}
+
+
+def objective(params, train_data, test_data):
+
+  lr = LogisticRegression()\
+    .setMaxIter(1000)\
+    .setRegParam(params['regParam'])\
+    .setFeaturesCol('Features')\
+    .setLabelCol('Survived')
+
+  evaluator = BinaryClassificationEvaluator()\
+    .setLabelCol('Survived')
+
+  lg_model = lr.fit(train_data)
+
+  auc = evaluator.evaluate(lg_model.transform(test_data))
+
+  with mlflow.start_run():
+    mlflow.log_params(params)
+    mlflow.log_metric('auc', auc)
+  
+  return {'loss': -auc, 'status': STATUS_OK}
+
 
 def main():
   logger.info("Creating Spark Session ...")
+  
   spark = SparkSession\
-      .builder\
-      .appName("pyspark_training")\
-      .getOrCreate()
+    .builder\
+    .master('local[*]')\
+    .appName('Spark ML Research')\
+    .config('spark.sql.repl.eagerEval.enabled', True) \
+    .config("spark.hadoop.fs.s3a.access.key", os.environ['AWS_ACCESS_KEY_ID']) \
+    .config("spark.hadoop.fs.s3a.secret.key", os.environ['AWS_SECRET_ACCESS_KEY']) \
+    .config('spark.hadoop.fs.s3.impl', 'org.apache.hadoop.fs.s3a.S3AFileSystem') \
+    .config('spark.yarn.dist.archives', 's3a://pyspark-venvs/mlflow-hyperopt-dataproc-2.1.18.tar.gz#venv')\
+    .getOrCreate()
     
   logger.info(spark)
 
-  df = spark.read.csv("s3a://mlops204-dataproc-bucket/data/titanic/train.csv", header=True, inferSchema=True)
+  df = spark.read.csv(
+    's3a://mlops204-dataproc-bucket/data/titanic/train.csv', 
+    header=True, 
+    inferSchema=True
+  )
+
+  df = df.select([
+    'Survived',
+    'Pclass',
+    'Sex',
+    'Age',
+    'SibSp',
+    'Parch',
+    'Fare',
+    'Embarked'
+  ]).na.drop()
 
   logger.info(df.limit(10).toPandas())
+
+  dataproc = data_prep_pipeline()
+
+  ready_data = dataproc.fit(df).transform(df)
+
+  train_data, test_data = ready_data.randomSplit([.7, .3])
+
+  trials = SparkTrials()
+
+  mlflow.set_experiment('classification')
+
+  best = fmin(
+    fn=partial(
+        objective, 
+        train_data=train_data,
+        test_data=test_data
+    ),
+    space=search_space,
+    algo=tpe.suggest,
+    max_evals=10,
+    trials=trials
+  )
 
 
 if __name__ == "__main__":
